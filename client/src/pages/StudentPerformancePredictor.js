e11fa82cadc3ee04ff8e824825d2a115335c110b
IBR4H33M <ibraheemibnanwar@gmail.com>
2026-04-04 23:42:35 +0600

    UI changes

--- client/src/pages/StudentPerformancePredictor.js
+++ client/src/pages/StudentPerformancePredictor.js
@@ -102,9 +102,6 @@ const StudentPerformancePredictor = () => {
             <div className="spp-container">
                 <div className="spp-inner">
                     <h2 className="spp-section-title">
-                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                            <path d="M22 12h-4l-3 9L9 3l-3 9H2"/>
-                        </svg>
                         ENTER STUDENT DATA
                     </h2>
 
